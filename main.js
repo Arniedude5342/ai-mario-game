b5abc0747e628589048b4a99c8a3ff67813d72a4
Arniedude5342 <85857848+Arniedude5342@users.noreply.github.com>
2023-08-16 10:36:03 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,61 +1,45 @@
-img = "";
-noseX = 0;
-noseY = 0;
-marioX = 325;
-marioY = 325;
-
 function preload() {
+	mario_gameover = loadSound("gameover.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
-	img = loadImage("mario05.png");
 }
 
 function setup() {
-	canvas = createCanvas(650, 400);
+	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
+
 	instializeInSetup(mario);
+	
 	video = createCapture(VIDEO);
-	video.size(800, 400);
-	video.parent('game_console')
+	video.size(800,400);
+	video.parent('game_console');
 
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
-}
 
-function draw() {
-	game()
-	background("#D3D3D3");
-	if(noseX < 300) 
-	{
-		marioX = marioX - 1;
-	}
-	if(noseX > 300) 
-	{
-		marioX = marioX +1;
-	}
-	if(noseY < 150) {
-		marioY = marioY -1;
-	}
-	image(img, marioX, marioY, 40, 70);
 }
 
 function modelLoaded() {
-	console.log('Model Loaded')
-}
+	console.log('Model Loaded!');
+  }
 
-function gotPoses(results)
-{
+  function gotPoses(results)
+  {
 	if(results.length > 0)
 	{
-		noseX = results[0].pose.nose.x;
-		noseY = results[0].pose.nose.y;
-		console.log("noseX = "+noseX +", noseY = "+noseY);
+	  console.log(results);
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
 	}
-}
-
-function game() {
-	
+  }
+   
+function draw() {
+	game();
 }
 
 
